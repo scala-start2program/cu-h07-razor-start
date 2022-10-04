9985abfb70430782c0212febff7db5bf914d5563
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2022-10-04 14:56:37 +0200

    Add project files.

---
+++ Wba.StovePalace/Pages/Stoves/Details.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Wba.StovePalace.Data;
+using Wba.StovePalace.Models;
+
+namespace Wba.StovePalace.Pages.Stoves
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Wba.StovePalace.Data.StoveContext _context;
+
+        public DetailsModel(Wba.StovePalace.Data.StoveContext context)
+        {
+            _context = context;
+        }
+
+      public Stove Stove { get; set; }
+
+        public  IActionResult OnGet(int? id)
+        {
+            if (id == null || _context.Stove == null)
+            {
+                return NotFound();
+            }
+
+            var stove =  _context.Stove
+                .Include(b => b.Brand)
+                .Include(f => f.Fuel).ToList()
+                .FirstOrDefault(m => m.Id == id);
+            if (stove == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Stove = stove;
+            }
+            return Page();
+        }
+    }
+}
